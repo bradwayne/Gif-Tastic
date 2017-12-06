5569fdfe0d76cf37e018f6886b0a78510ac0bb44
unknown <bradwayne74@gmail.com>
2017-12-06 12:47:33 -0500

    working without train list

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,113 +1,137 @@
+  $(document).ready(function() {
 
-var gifTopics = ['kittens', 'puppies', 'owls', 'bunnies', 'fish', 'sharks', 'chinchillas',
-    'bats', 'hedgehog', 'cows', 'birds', 'turtles', 'frogs', 'lizards',
-    'skunks', 'ferrets', 'pigs'
-];
+      function runProgram() {
 
-
-$("body").on("click", "button", function() {
-    // Grabbing and storing the data property value from the button
-    var topic = $(this).attr("data-topic");
-
-    // Constructing a queryURL using the topic name
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        topic + "&api_key=696K8TVeuBJBg1ref0LA24jIoa4gZspp&limit=10";
+          var database;
 
 
-    // Performing an AJAX request with the queryURL
-    $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        // After data comes back from the request
-        .done(function(response) {
-            console.log(queryURL);
-            console.log(response);
+// firebase api key
+          var config = {
+              apiKey: "AIzaSyC16dFr-JCQ5HpTL2ndW6bPjPQpArYt_vo",
+              authDomain: "bws-anytime-train.firebaseapp.com",
+              databaseURL: "https://bws-anytime-train.firebaseio.com",
+              projectId: "bws-anytime-train",
+              storageBucket: "bws-anytime-train.appspot.com",
+              messagingSenderId: "77875475122"
+          };
 
-            $("#gifs-here").empty();
-
-
-            // storing the data from the AJAX request in the results variable
-            var results = response.data;
-            // Looping through each result item
-            for (var i = 0; i < results.length; i++) {
-                // Creating and storing a div tag
-                var topicDiv = $("<div class='floatLeft'></div>");
-                // Creating a paragraph tag with the result item's rating
-                var p = $("<p>").text("Rating: " + results[i].rating);
-                // Creating and storing an image tag
-                var gifImg = $("<img width='225'>");
-               
-                // Setting the src attribute of the image to a property pulled off the result item
-                gifImg.attr('src', results[i].images.fixed_height_small_still.url);
-
-                gifImg.attr('data-animate', results[i].images.fixed_height_small.url);
-
-                gifImg.attr('data-state', 'still');
-
-                gifImg.attr('data-still', results[i].images.fixed_height_small_still.url);
+//firebase initalizer
+          firebase.initializeApp(config);
 
 
+          var database = firebase.database();
 
-                // Appending the paragraph and image tag to the animalDiv
-                topicDiv.append(p);
-                topicDiv.append(gifImg);
+// add train on click fuction
+          $("#add-loco").on("click", function() {
+// do not clear entry
+              event.preventDefault();
 
-                // Prependng the topicDiv to the HTML page in the "#gifs-here" div
-                $("#gifs-here").prepend(topicDiv);
-            }
-        });
-});
+// take in input form user and remove extra spacing
+              var train = $("#train-input").val().trim();
+              var destination = $("#destination-input").val().trim();
+              var nextTrain = $("#nextTrain-input").val().trim();
+              var frequency = $("#frequency-input").val().trim();
 
+// variables for train fields
+              var newTrain = {
+                  trainName: train,
+                  trainDestination: destination,
+                  trainNextTrain: nextTrain,
+                  trainFrequency: frequency
+              };
 
-// Function for displaying topics data
-function renderButtons() {
-    // Deleting the topics buttons prior to adding new topics buttons
-    // (this is necessary otherwise we will have repeat buttons)
-    $("#button-view").empty();
-    // Looping through the array of topics
-    for (var i = 0; i < gifTopics.length; i++) {
-        // Then dynamicaly generating buttons for each topics in the array.
-        // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-        var a = $("<button class='ma-sm btn btn-success'>");
-        // Adding a class
-        a.addClass("topic");
-        // Adding a data-attribute with a value of the topics at index i
-        a.attr("data-topic", gifTopics[i]);
-        // Providing the button's text with a value of the topics at index i
-        a.text(gifTopics[i]);
-        // Adding the button to the HTML
-        $("#button-view").append(a);
-    }
-}
-// This function handles events where one button is clicked
-$("#add-topic").on("click", function(event) {
-    // event.preventDefault() prevents the form from trying to submit itself.
-    // We're using a form so that the user can hit enter instead of clicking the button if they want
-    event.preventDefault();
-    // This line will grab the text from the input box
-    var topic = $(".form-control").val().trim();
-    // The topics from the textbox is then added to our array
-    gifTopics.push(topic);
-    // calling renderButtons which handles the processing of our topics array
-    renderButtons();
-});
+// push fields into form table
+              database.ref().push(newTrain);
 
+              console.log(newTrain.trainName);
+              console.log(newTrain.trainDestination);
+              console.log(newTrain.trainNextTrain);
+              console.log(newTrain.trainFrequency);
 
-// create the on click function for the still and animate
-$("body").on("click", "img", function() {
+// alert after submit train info
+              alert("Next Train Now In Route");
 
-var gifImg = $(this);
-      var state = gifImg.attr("data-state");
+// clear form field
+              $("#train-input").val("");
+              $("#destination-input").val("");
+              $("#nextTrain-input").val("");
+              $("#frequency-input").val("");
 
-      if (state === "still") {
-        gifImg.attr("src", gifImg.attr("data-animate"));
-        gifImg.attr("data-state", "animate");
-      } else {
-        gifImg.attr("src", gifImg.attr("data-still"));
-        gifImg.attr("data-state", "still");
+          });
+
+// variable for firebase input
+          database.ref().on("value", function(snapshot) {
+              console.log(snapshot.val());
+
+            var UpdateHtml = {
+              train: snapshot.val().trainName,
+              destination: snapshot.val().trainDestination,
+              nextTrain: snapshot.val().trainNextTrain,
+              frequency: snapshot.val().trainFrequency,
+          }
+
+          });
+
       }
-    });
 
-// Calling the renderButtons function at least once to display the initial list of movies
-renderButtons();
+
+      function UpdateHtml(trian, destination, frequency, nextTrain) {
+
+          var row;
+          var train;
+          var tDestination;
+          var tFrequency;
+          var tArrival;
+          var tMinsOut;
+          var nextArrival;
+          var minsOut;
+
+          nextArrival = calcNextArrival(nextTrain, frequency);
+          tMinsOut = calacMInAway(nextArrival);
+          row = $("<tr>");
+          train = $("<td>").text(name);
+          tDestination = $("<td>").text(destination);
+          tFrequency = $("<td>").text(frequency);
+          tArrival = $("<td>").text(nextArrival);
+          minsOut = $("<td>").text(tMinsOut);
+
+          row.append(train);
+          row.append(tDestination);
+          row.append(tFrequency);
+          row.append(minsOut);
+          row.append(tArrival)
+          $("#train-info-section").append(row);
+      }
+
+      function calcNextArrival(nextTrain, frequency) {
+
+          var firstTrainIn;
+          var currentTime;
+          var diffTime;
+          var tRemaining;
+          var tMinutesNextTrain;
+
+          firstTrainIn = moment(nextTrain, "hh:mm").substact(1, "years");
+          currentTime = moment();
+          diffTime = moment().diff(moment(firstTrainIn), "minutes");
+          tRemaining = diffTime % frequency;
+          tMinutesNextTrain = frequency - tRemaining;
+
+          return tMinutesNextTrain;
+
+      }
+
+      function CalcMinsAway(tMinutesNextTrain) {
+
+          var minsOut;
+
+          minsOut = moment().add(tMinutesNextTrain, "minutes");
+          moment(minsOut).format("hh:mm");
+
+          return minsOut;
+      }
+
+  runProgram();
+
+
+  });
